6b720c3c84bd420a1666c8146a1089c89725427e
freejob985 <freejob985@gmail.com>
2020-12-29 01:21:02 +0200

    ssssw2

--- .history/resources/views/backend/user/partials/sidebar.blade_20201229012026.php
+++ .history/resources/views/backend/user/partials/sidebar.blade_20201229012026.php
@@ -101,7 +101,7 @@ $subscription = $login_user->subscription()->get()->first();
     </li>
    @endif
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('user.subscriptions.edit', $subscription->id) }}">
+        <a class="nav-link" href="{{ route('user.subscriptions.index') }}">
             <i class="far fa-credit-card"></i>
             <span>{{ __('backend.sidebar.subscription') }}</span></a>
     </li>
